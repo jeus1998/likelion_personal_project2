5dc47165d56ca69e49f1596bff23c5fbc603be63
baejeu12 <baejeu@naver.com>
2023-08-03 15:20:20 +0900

    project2 start build.gradle / yaml 추가

---
+++ src/main/java/com/example/project_2_baejewoo/Project2BaeJeWooApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_2_baejewoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2BaeJeWooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2BaeJeWooApplication.class, args);
+	}
+
+}
